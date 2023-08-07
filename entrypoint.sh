aa290f71b005fd49436e40824962856f87f09319
solo5star <solo_5@naver.com>
2023-08-07 21:50:10 +0900

    스크립트 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-touch ~/.ssh/known_hosts
+rm -rf ~/.ssh
+mkdir /ssh 2> /dev/null
+cp -R /ssh ~/.ssh
+
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/*
-chmod u+w ~/.ssh/known_hosts
 
 echo TUNNEL_HOST=$TUNNEL_HOST
 echo REMOTE_HOST=$REMOTE_HOST
 echo REMOTE_PORT=$REMOTE_PORT
 echo LOCAL_PORT=$LOCAL_PORT
 
-exec ssh -t -t -L *:$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $TUNNEL_HOST 1> /dev/null
+exec ssh -t -t \
+  -o StrictHostKeyChecking=no \
+  -L *:$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT \
+  $TUNNEL_HOST \
+  1> /dev/null
